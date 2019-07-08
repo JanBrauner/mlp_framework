9d8ba29f797b49abfcd1544d5b008d2f59cea05f
Jan Brauner <jan.brauner@gmx.de>
2019-07-08 12:05:51 +0100

    xxx

--- cluster_experiment_scripts/CE_test_cluster.sh
+++ cluster_experiment_scripts/CE_test_cluster.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
-#SBATCH --partition=Standard
-#SBATCH --gres=gpu:1
+#SBATCH --partition=Interactive
+#SBATCH --gres=gpu:4
 #SBATCH --mem=12000  # memory in Mb
-#SBATCH --time=0-08:00:00
+#SBATCH --time=0-02:00:00
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
 
@@ -35,9 +35,4 @@ export DATASET_DIR=${TMP}/datasets/
 
 source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
 cd ..
-python train_evaluate_emnist_classification_system.py --batch_size 100 --continue_from_epoch -1 --seed 0 \
-                                                      --image_num_channels 1 --image_height 28 --image_width 28 \
-                                                      --dim_reduction_type "strided" --num_layers 4 --num_filters 64 \
-                                                      --num_epochs 100 --experiment_name 'emnist_test_exp' \
-                                                      --use_gpu "True" --gpu_id "0" --weight_decay_coefficient 0. \
-                                                      --dataset_name "emnist"
+python main.py --experiment_name 'CE_test_cluster'
