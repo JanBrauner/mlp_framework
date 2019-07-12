0550c43c12d93029b74111218d83e70086688139
Jan Brauner <jan.brauner@gmx.de>
2019-07-12 11:42:22 +0100

    xxx

--- mias_data_preprocessing.py
+++ mias_data_preprocessing.py
@@ -79,21 +79,19 @@ file_names_healthy_train_set = file_names_healthy[:int(healthy_split[0]*len(file
 file_names_healthy_val_set = file_names_healthy[int(healthy_split[0]*len(file_names_healthy)) : int((healthy_split[0]+healthy_split[1])*len(file_names_healthy))]
 file_names_healthy_test_set = file_names_healthy[int((healthy_split[0]+healthy_split[1])*len(file_names_healthy)):]
 
-# =============================================================================
-# #%% Healthy: unprocessed
-# # copy files from raw to respective folders
-# for file_name in file_names_healthy_train_set:
-#     copy2(os.path.join(raw_path, file_name + ".pgm"), os.path.join(base_path_healthy, "train"))    
-# 
-# for file_name in file_names_healthy_val_set:
-#     copy2(os.path.join(raw_path, file_name + ".pgm"), os.path.join(base_path_healthy, "val"))
-#     
-# for file_name in file_names_healthy_test_set:
-#     copy2(os.path.join(raw_path, file_name + ".pgm"), os.path.join(base_path_healthy, "test"))
-# 
-# =============================================================================
+#%% Healthy: unprocessed
+# copy files from raw to respective folders
+for file_name in file_names_healthy_train_set:
+    copy2(os.path.join(raw_path, file_name + ".pgm"), os.path.join(base_path_healthy, "train"))    
 
-#%% Healthy: processed
+for file_name in file_names_healthy_val_set:
+    copy2(os.path.join(raw_path, file_name + ".pgm"), os.path.join(base_path_healthy, "val"))
+    
+for file_name in file_names_healthy_test_set:
+    copy2(os.path.join(raw_path, file_name + ".pgm"), os.path.join(base_path_healthy, "test"))
+
+
+#%% Healthy: processed (binary segmenation, then only keep leargest connected component and crop image accordingly)
 from skimage import measure
 import copy
 
@@ -143,68 +141,66 @@ for file_name in file_names_healthy_test_set:
     processed_image.save(os.path.join(base_path_healthy_processed, "test", file_name) + ".pgm")
 
 
-# =============================================================================
-# #%% calculate mean and SD values for normalisation:
-# train_images_healthy = os.listdir(os.path.join(base_path_healthy, "train"))
-# 
-# image_stack = np.empty((num_healthy, 1024, 1024)) # this approach only works because the set is small
-# transform = transforms.ToTensor() # same transforms used in training
-# 
-# # sequentially load all images and add pixel values to image_stack as np.arrays
-# for i,file_name in enumerate(train_images_healthy):
-#     image = Image.open(os.path.join(base_path_healthy, "train", file_name))
-#     tensor = transform(image)
-#     image_stack[i,:,:] = tensor.numpy()
-# 
-# # calculate overall image stats
-# mn = np.mean(image_stack)   
-# sd = np.std(image_stack)
-# 
-# print("Mean and SD of all healthy training images (full image): ", mn, sd)
-# 
-# # calculate overall image for central region only
-# def create_central_region_slice(image_size, size_central_region):
-#     margins = ((image_size[1]-size_central_region[0])/2, 
-#                (image_size[2]-size_central_region[1])/2) # size of margins in dimensions 1 and 2 (relative to the 3-D tensor) between the image borders and the patch borders
-#     
-#     central_region_slice = np.s_[:, 
-#                       math.ceil(margins[0]):math.ceil(image_size[1]-margins[0]), 
-#                       math.ceil(margins[1]):math.ceil(image_size[2]-margins[1])]
-#     return central_region_slice
-# 
-# central_region_slice = create_central_region_slice((1,1024,1024), (256,256))
-# 
-# mn_central = np.mean(image_stack[central_region_slice]) 
-# sd_central = np.std(image_stack[central_region_slice])
-# print("Mean and SD of all healthy training images (central region only): ", mn_central, sd_central)
-# 
-# 
-# #%% prepare pathological folder
-# # select pathological images only
-# df_pathol = df[df.class_of_abnormality != "NORM"]
-# 
-# # copy files of input images to test folder (only test set here)
-# for file_name in df_pathol.index:
-#     copy2(os.path.join(raw_path, file_name + ".pgm"), os.path.join(base_path_pathol, "test", "images"))
-#     
-# # create and save binary segmentation target images
-# for image_name in df_pathol.index:
-#     bounding_box_coordinates = [df_pathol.loc[image_name,"y"],
-#                                 df_pathol.loc[image_name,"x"],
-#                                 df_pathol.loc[image_name,"radius"]] 
-#     if type(bounding_box_coordinates[0]) is pd.core.series.Series: # This is to deal with the few cases where there are 2 or more rows for a single patient. The target image in this case combines all segmentation labels.
-#         bounding_box_coordinates = [bounding_box_coordinates[i].values for i in range(3)]
-#         masks = []
-#         for i in range(len(bounding_box_coordinates[0])):
-#             masks.append(create_target_image([bounding_box_coordinates[j][i] for j in range(3)]))
-#         target_image = np.maximum(masks[0],masks[1])
-#         if len(masks) > 2:
-#             for mask in masks[2:len(masks)+1]:
-#                 target_image = np.maximum(target_image, mask)
-#     else:
-#         target_image = create_target_image(bounding_box_coordinates)
-#         
-#     target_image = Image.fromarray(np.uint8(target_image*255))
-#     target_image.save(os.path.join(base_path_pathol, "test", "target_images",image_name) + ".pgm")
-# 
-# =============================================================================
+#%% calculate mean and SD values for normalisation:
+train_images_healthy = os.listdir(os.path.join(base_path_healthy, "train"))
+
+image_stack = np.empty((num_healthy, 1024, 1024)) # this approach only works because the set is small
+transform = transforms.ToTensor() # same transforms used in training
+
+# sequentially load all images and add pixel values to image_stack as np.arrays
+for i,file_name in enumerate(train_images_healthy):
+    image = Image.open(os.path.join(base_path_healthy, "train", file_name))
+    tensor = transform(image)
+    image_stack[i,:,:] = tensor.numpy()
+
+# calculate overall image stats
+mn = np.mean(image_stack)   
+sd = np.std(image_stack)
+
+print("Mean and SD of all healthy training images (full image): ", mn, sd)
+
+# calculate overall image for central region only
+def create_central_region_slice(image_size, size_central_region):
+    margins = ((image_size[1]-size_central_region[0])/2, 
+               (image_size[2]-size_central_region[1])/2) # size of margins in dimensions 1 and 2 (relative to the 3-D tensor) between the image borders and the patch borders
+    
+    central_region_slice = np.s_[:, 
+                      math.ceil(margins[0]):math.ceil(image_size[1]-margins[0]), 
+                      math.ceil(margins[1]):math.ceil(image_size[2]-margins[1])]
+    return central_region_slice
+
+central_region_slice = create_central_region_slice((1,1024,1024), (256,256))
+
+mn_central = np.mean(image_stack[central_region_slice]) 
+sd_central = np.std(image_stack[central_region_slice])
+print("Mean and SD of all healthy training images (central region only): ", mn_central, sd_central)
+
+
+#%% prepare pathological folder
+# select pathological images only
+df_pathol = df[df.class_of_abnormality != "NORM"]
+
+# copy files of input images to test folder (only test set here)
+for file_name in df_pathol.index:
+    copy2(os.path.join(raw_path, file_name + ".pgm"), os.path.join(base_path_pathol, "test", "images"))
+    
+# create and save binary segmentation target images
+for image_name in df_pathol.index:
+    bounding_box_coordinates = [df_pathol.loc[image_name,"y"],
+                                df_pathol.loc[image_name,"x"],
+                                df_pathol.loc[image_name,"radius"]] 
+    if type(bounding_box_coordinates[0]) is pd.core.series.Series: # This is to deal with the few cases where there are 2 or more rows for a single patient. The target image in this case combines all segmentation labels.
+        bounding_box_coordinates = [bounding_box_coordinates[i].values for i in range(3)]
+        masks = []
+        for i in range(len(bounding_box_coordinates[0])):
+            masks.append(create_target_image([bounding_box_coordinates[j][i] for j in range(3)]))
+        target_image = np.maximum(masks[0],masks[1])
+        if len(masks) > 2:
+            for mask in masks[2:len(masks)+1]:
+                target_image = np.maximum(target_image, mask)
+    else:
+        target_image = create_target_image(bounding_box_coordinates)
+        
+    target_image = Image.fromarray(np.uint8(target_image*255))
+    target_image.save(os.path.join(base_path_pathol, "test", "target_images",image_name) + ".pgm")
+
